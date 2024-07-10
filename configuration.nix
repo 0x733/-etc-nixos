8e2a7cac4b4c1fdae085277c0bdb5f5a4638e380
Root <root@localhost.localdomain>
2024-07-10 17:17:03 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -134,7 +134,7 @@ programs = {
       enableCompletion = true;
       ohMyZsh = {
          enable = true;
-         theme = "robbyrussell";
+         theme = "bira";
          plugins = [
            "git"
            "sudo"
@@ -150,7 +150,6 @@ programs = {
 
 
 programs.kdeconnect.enable = true;
-
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
